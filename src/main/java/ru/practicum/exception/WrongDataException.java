8ebd674f6d477e37681737761de9eb1bd370f2b2
Maria <pavlov303@yandex.ru>
2023-08-04 08:05:13 +0300

    fix STILE

--- src/main/java/ru/practicum/exception/WrongDataException.java
+++ src/main/java/ru/practicum/exception/WrongDataException.java
@@ -1,6 +1,6 @@
 package ru.practicum.exception;
 
-public class WrongDataException extends RuntimeException{
+public class WrongDataException extends RuntimeException {
     public WrongDataException(String message) {
         super(message);
     }
--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
 
     @Id
